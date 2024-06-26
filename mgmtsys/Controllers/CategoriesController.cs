f258f30d7b2bcc331c27fc8a12cbd39fcdd67e3a
divyanshraj467 <divyanshraj1@gmail.com>
2024-06-27 02:54:03 +0530

    Add project files.

---
+++ mgmtsys/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using mgmtsys.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace mgmtsys.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepo.GetCategories();
+            return View(categories);
+        }
+        public IActionResult Edit(int? id)
+        {
+            var category = CategoriesRepo.GetCategoryById(id.HasValue?id.Value:0);
+           return View(category);
+
+        }
+        public IActionResult Edit(Category category)
+        {
+        }   
+    }
+}
